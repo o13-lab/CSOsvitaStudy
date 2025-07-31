42fa0b278b6877d67055a6599ae7756568748a52
Oleh Kuzmenko <Oleg.Kuzmenko@fuib.com>
2025-07-31 23:09:46 +0300

    Finished 1st assignment.

--- src/main/java/study/cs/hw_1/TimeComplexity.java
+++ src/main/java/study/cs/hw_1/TimeComplexity.java
@@ -2,6 +2,8 @@ package study.cs.hw_1;
 
 class TimeComplexity {
 
+    private static final double GAMMA = 0.5772156649015328606;
+
     int assignmentA(int n) {
         return (n - 2) / 10;
     }
@@ -11,11 +13,7 @@ class TimeComplexity {
     }
 
     int assignmentC(int n) {
-        int sqrt = (int) Math.sqrt(n);
-        if (sqrt * sqrt < n) {
-            return sqrt + 1;
-        }
-        return sqrt;
+        return (int) Math.sqrt(n - 1) + 1;
     }
 
     int assignmentD(int n) {
@@ -27,11 +25,34 @@ class TimeComplexity {
     }
 
     int assignmentF(int n) {
-        int m = (int) (Math.log(n - 1) / Math.log(2));
-        return n * (m + 1) - (int) Math.pow(2, m + 1) + 1;
+        int sum = 0;
+        for (int k = 1; k < n; k++) {
+            sum += (int) (Math.log(k) / Math.log(2));
+        }
+        return sum + (n - 1);
     }
 
     int assignmentG(int n) {
         return n * (n + 1) * (n + 2) / 6;
+    }
+
+    int assignmentH(int n) {
+        return getHarmonicSum(n) + n * (n + 1) / 2;
+    }
+
+    private int getHarmonicSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += (int) Math.ceil((double) n / i);
+        }
+        return sum;
+    }
+
+    int assignmentI(int n) {
+        return 2 * n - Integer.bitCount(n);
+    }
+
+    int assignmentJ(int n) {
+        return n * (int) (Math.log(n) / Math.log(2)) + n;
     }
 }
--- src/test/java/study/cs/hw_1/TimeComplexityTest.java
+++ src/test/java/study/cs/hw_1/TimeComplexityTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -51,7 +52,7 @@ class TimeComplexityTest {
 
     @Test
     void assignmentCShouldCalculateCounterCorrectly() {
-        random.ints(0, 10_000)
+        random.ints(1, 10_000)
                 .limit(1000)
                 .forEach(n -> assertEquals(assignmentCVerifier(n), timeComplexity.assignmentC(n)));
     }
@@ -67,8 +68,8 @@ class TimeComplexityTest {
     @Test
     void assignmentDShouldCalculateCounterCorrectly() {
         random.ints(0, 10_000)
-            .limit(1000)
-            .forEach(n -> assertEquals(assignmentDVerifier(n), timeComplexity.assignmentD(n)));
+                .limit(1000)
+                .forEach(n -> assertEquals(assignmentDVerifier(n), timeComplexity.assignmentD(n)));
     }
 
     private int assignmentDVerifier(int n) {
@@ -84,8 +85,8 @@ class TimeComplexityTest {
     @Test
     void assignmentEShouldCalculateCounterCorrectly() {
         random.ints(0, 100)
-            .limit(10)
-            .forEach(n -> assertEquals(assignmentEVerifier(n), timeComplexity.assignmentE(n)));
+                .limit(10)
+                .forEach(n -> assertEquals(assignmentEVerifier(n), timeComplexity.assignmentE(n)));
     }
 
     private int assignmentEVerifier(int n) {
@@ -101,8 +102,8 @@ class TimeComplexityTest {
     @Test
     void assignmentFShouldCalculateCounterCorrectly() {
         random.ints(1, 100)
-            .limit(10)
-            .forEach(n -> assertEquals(assignmentFVerifier(n), timeComplexity.assignmentF(n)));
+                .limit(100)
+                .forEach(n -> assertEquals(assignmentFVerifier(n), timeComplexity.assignmentF(n)));
     }
 
     private int assignmentFVerifier(int n) {
@@ -118,8 +119,8 @@ class TimeComplexityTest {
     @Test
     void assignmentGShouldCalculateCounterCorrectly() {
         random.ints(0, 100)
-            .limit(10)
-            .forEach(n -> assertEquals(assignmentGVerifier(n), timeComplexity.assignmentG(n)));
+                .limit(100)
+                .forEach(n -> assertEquals(assignmentGVerifier(n), timeComplexity.assignmentG(n)));
     }
 
     private int assignmentGVerifier(int n) {
@@ -132,6 +133,74 @@ class TimeComplexityTest {
             }
         }
         return counter;
+    }
+
+    @Test
+    void assignmentHShouldCalculateCounterCorrectly() {
+        random.ints(0, 1000)
+                .limit(100)
+                .forEach(n -> assertEquals(assignmentHVerifier(n), timeComplexity.assignmentH(n)));
+    }
+
+    private int assignmentHVerifier(int n) {
+        int counter = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j += i) {
+                counter++;
+            }
+            for (int j = 1; j <= i; j++) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+
+    @Test
+    void assignmentIShouldCalculateCounterCorrectly() {
+        random.ints(0, 10)
+                .limit(100)
+                .forEach(n -> {
+                    final AtomicInteger iCounter = new AtomicInteger();
+                    assignmentIVerifier(n, iCounter);
+                    assertEquals(iCounter.get(), timeComplexity.assignmentI(n));
+                });
+    }
+
+
+    private void assignmentIVerifier(int n, AtomicInteger iCounter) {
+        if (n == 0) {
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            iCounter.incrementAndGet();
+        }
+        assignmentIVerifier(n / 2, iCounter);
+    }
+
+    @Test
+    void assignmentJShouldCalculateCounterCorrectly() {
+        random.ints(0, 10)
+                .limit(100)
+                .map(n -> (int) Math.pow(2, n))
+                .forEach(n -> {
+                    final AtomicInteger jCounter = new AtomicInteger();
+                    assignmentJVerifier(n, jCounter);
+                    assertEquals(jCounter.get(), timeComplexity.assignmentJ(n));
+                });
+    }
+
+    private void assignmentJVerifier(int n, AtomicInteger jCounter) {
+        if (n == 0) {
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            jCounter.incrementAndGet();
+        }
+
+        assignmentJVerifier(n/2, jCounter);
+        assignmentJVerifier(n/2, jCounter);
     }
 
 }
